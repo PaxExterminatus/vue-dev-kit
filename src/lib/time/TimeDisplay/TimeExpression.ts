35ead1e53c62bb4ed003291f399063b5d5c18f49
VoroninV <paxexterminatus@gmail.com>
2019-08-05 17:56:47 +0300

    time diff add now

--- src/lib/time/TimeDisplay/TimeExpression.ts
+++ src/lib/time/TimeDisplay/TimeExpression.ts
@@ -1,24 +1,31 @@
 import moment, {MomentInput, Moment} from 'moment'
 
-export abstract class TimeExpression {
+export abstract class TimeExpression
+{
     readonly second = 1000;
     readonly minute = this.second * 60;
     readonly hour   = this.minute * 60;
-    readonly day    = this.hour * 24;
-    readonly month  = this.day * 30;
-    readonly year   = this.month * 12;
-    moment: Moment;
-    constructor(inp: MomentInput) {
+    readonly day    = this.hour   * 24;
+    readonly month  = this.day    * 30;
+    readonly year   = this.month  * 12;
+
+    readonly now : Moment = moment();
+    readonly moment : Moment;
+
+    constructor(inp: MomentInput)
+    {
         this.moment = moment(inp);
     }
 
-    get diff() : number {
-        return moment().diff(this.moment)
+    get diff() : number
+    {
+        return this.now.diff(this.moment)
     }
 
     abstract display() : string | undefined
 }
 
-export interface TimeExpressionClass {
+export interface TimeExpressionClass
+{
     new (inp: MomentInput): TimeExpression;
 }
--- src/lib/time/TimeExpression.vue
+++ src/lib/time/TimeExpression.vue
@@ -1,7 +1,5 @@
 <template>
-    <time class="time-expression">
-        {{display}}
-    </time>
+    <time class="time-expression">{{display}}</time>
 </template>
 
 <script>
