0ce66917d6f9b9ecd078aafd073dac8055d9d119
VoroninV <paxexterminatus@gmail.com>
2019-09-25 17:36:04 +0300

    clear

--- src/views/Input.vue
+++ src/views/Input.vue
@@ -2,27 +2,19 @@
   <div class="about">
     <h1>User data input fields</h1>
     <section>
-      <h2>Input multiline \n</h2>
-      <input-multiline @input="searchInput"/>
-      <div>
-        {{searchArray}}
-      </div>
+
     </section>
   </div>
 </template>
 
 <script>
-import inputMultiline from 'in/input-multiline'
 export default {
-  components: {inputMultiline},
   data(){return{
     searchArray: '',
   }},
 
-  methods:{
-    searchInput(input) {
-      this.searchArray = input;
-    }
+  methods: {
+
   }
 }
 </script>
